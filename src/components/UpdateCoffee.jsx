4023641a6df4f0119d17b3ed25c6ee0266b1e572
AhanafAtif40 <git config --global user.email mdahanafatifs@gmail.com>
2025-05-23 15:34:25 +0600

    added the coffee server

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
+
+const UpdateCoffee = () => {
+  const { _id, name, quantity, price, taste, supplier, photo, details } =
+    useLoaderData();
+  const handleUpdateCoffee = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const updatedCoffee = Object.fromEntries(formData.entries());
+    console.log(updatedCoffee);
+
+    // send updated coffee data to the db
+    fetch(`http://localhost:3000/coffees/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Coffee updated successfully!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  return (
+    <div className="p-24">
+      <div className="p-12 text-center space-y-4">
+        <h1 className="text-6xl">Update Coffee</h1>
+      </div>
+      <form onSubmit={handleUpdateCoffee}>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Name</label>
+            <input
+              type="text"
+              name="name"
+              defaultValue={name}
+              className="input w-full"
+              placeholder="Enter Coffee Name"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Quantity</label>
+            <input
+              type="text"
+              name="quantity"
+              defaultValue={quantity}
+              className="input w-full"
+              placeholder="Enter Quantity chef"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Supplier</label>
+            <input
+              type="text"
+              name="supplier"
+              defaultValue={supplier}
+              className="input w-full"
+              placeholder="Enter Coffee Supplier"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Taste</label>
+            <input
+              type="text"
+              name="taste"
+              defaultValue={taste}
+              className="input w-full"
+              placeholder="Enter coffee taste"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Price</label>
+            <input
+              type="text"
+              name="price"
+              defaultValue={price}
+              className="input w-full"
+              placeholder="Enter coffee Price per Cup"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Details</label>
+            <input
+              type="text"
+              name="details"
+              defaultValue={details}
+              className="input w-full"
+              placeholder="Enter coffee details"
+            />
+          </fieldset>
+        </div>
+
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border my-4 p-4">
+          <label className="label">Photo</label>
+          <input
+            type="text"
+            name="photo"
+            defaultValue={photo}
+            className="input w-full"
+            placeholder="Enter photo URL"
+          />
+        </fieldset>
+        <input type="submit" value="Update Coffee" className="btn w-full" />
+      </form>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
